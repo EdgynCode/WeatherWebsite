1725c68f8cf77ac44167505481d0b15fd862017f
EdgynCode <nguyenhuutan080603@gmail.com>
2025-07-05 14:20:14 +0700

    remove unnecessary folders

---
+++ WeatherWebsiteAPI/Models/ForecastData.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace WeatherWebsite.Models
+{
+    public class ForecastData
+    {
+        [JsonProperty("cod")]
+        public string? Cod { get; set; }
+
+        [JsonProperty("message")]
+        public int? Message { get; set; }
+
+        [JsonProperty("cnt")]
+        public int? Cnt { get; set; }
+
+        [JsonProperty("list")]
+        public List<WeatherData>? List { get; set; }
+    }
+}
